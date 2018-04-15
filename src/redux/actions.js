653cd013ac84c788015d27f184c156acf975b49b
winchen <wchen0@gmail.com>
2018-04-15 14:58:36 -0400

    add basic add/update/delete for nodes reducer

---
+++ src/redux/actions.js
@@ -0,0 +1,9 @@
+const ADD_NODE = 'gp/add_node';
+const UPDATE_NODE = 'gp/update_node';
+const DELETE_NODE = 'gp/delete_node';
+
+export const Atype = {
+  ADD_NODE,
+  UPDATE_NODE,
+  DELETE_NODE
+}
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,58 @@
+import { combineReducers } from 'redux';
+import { Atype } from './actions';
+
 const INIT_STATE = {};
 
-const reducer = (state = INIT_STATE, action) => {
+const initNode = (
+  id = undefined,
+  data = {},
+  incoming = [],
+  outgoing = []
+) => ({ id, data, incoming, outgoing });
+
+const initEdge = (
+  id = undefined,
+  data = {},
+  origin = undefined,
+  destin = undefined
+) => ({ id, data, origin, destin });
+
+const nodeData = (data = {}, action) => {
+  switch (action.type) {
+    case Atype.UPDATE_NODE: {
+      return action.data; // NOTE: placeholder
+    }
+    default: return data;
+  }
+}
+
+const nodes = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case Atype.ADD_NODE: {
+      const { id, data, incoming, outgoing } = action;
+      const node = initNode(id, data, incoming, outgoing);
+      return Object.assign({}, state, { [id]: node});
+    }
+    case Atype.UPDATE_NODE: {
+      const { id, data } = action;
+      let node = state[id];
+      if (node) node = Object.assign({}, node, { data: nodeData(node.data, action) });
+      return Object.assign({}, state, { [id]: node});
+    }
+    case Atype.DELETE_NODE: {
+      const nState = Object.assign({}, state);
+      delete nState[action.id];
+      return nState;
+    }
+    default: return state;
+  }
+}
+
+const edges = (state = INIT_STATE, action) => {
   return state
 }
 
-export default reducer;
+export default combineReducers({
+  nodes,
+  edges
+});
