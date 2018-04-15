3ec69a98c881fc84c7038f5748d3eafef96fc7b0
winchen <wchen0@gmail.com>
2018-04-15 16:43:14 -0400

    add basic node actions and related tests

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,6 +6,26 @@ const ADD_EDGE = 'gp/add_edge';
 const UPDATE_EDGE = 'gp/update_edge';
 const DELETE_EDGE = 'gp/delete_edge';
 
+const addNode = node => ({
+  type: ADD_NODE,
+  ...node
+})
+const deleteNode = node => ({
+  type: DELETE_NODE,
+  ...node
+})
+const updateNode = ({ id, data }) => ({
+  type: UPDATE_NODE,
+  id,
+  data
+})
+
+
+const Action = {
+  addNode,
+  deleteNode,
+  updateNode
+}
 
 export const Atype = {
   ADD_NODE,
@@ -16,3 +36,5 @@ export const Atype = {
   UPDATE_EDGE,
   DELETE_EDGE
 }
+
+export default Action;
--- src/redux/reducers.test.js
+++ src/redux/reducers.test.js
@@ -1,5 +1,5 @@
 import reducers from './reducers';
-import { Atype } from './actions';
+import Action, { Atype } from './actions';
 import expect from 'expect';
 
 describe('graph actions and reducer', () => {
@@ -12,19 +12,87 @@ describe('graph actions and reducer', () => {
   it(`should handle ${ Atype.ADD_NODE }`, () => {
     const node = {
       id: 'node123',
-      data: { helloWorld: "hello" },
+      data: { helloWorld: 'hello' },
     }
-    const addNode = Object.assign({}, node, { type: Atype.ADD_NODE });
+    const addNode = Action.addNode(node);
     const expectedNode = Object.assign({}, node, { outgoing: [], incoming: [] });
 
+    /* add a node */
     expect(reducers(undefined, addNode)).toEqual({
       nodes: {
         [node.id]: expectedNode
       },
       edges: {}
     })
+
+    /* add nodes in succession */
+    const node2 = {
+      id: 'node234',
+      data: { anotherNode: 'hello' },
+      outgoing: [],
+      incoming: ['asdf']
+    }
+    const addTwoNodes = [node, node2].map(Action.addNode);
+
+    expect(addTwoNodes.reduce(reducers, undefined)).toEqual({
+      nodes: {
+        [node.id]: expectedNode,
+        [node2.id]: node2
+      },
+      edges: {}
+    })
   });
-  // it(`should handle UPDATE_POST_SUCCESS`);
-  // it(`should handle GET_POST_FAIL`);
-  // it(`should handle GET_POST_START`);
+  it(`should handle ${ Atype.UPDATE_NODE }`, () => {
+    const node = {
+      id: 'node123',
+      data: { helloWorld: 'hello' },
+      incoming: [],
+      outgoing: []
+    }
+
+    const updatedData = { byebye: 'baby' };
+
+    const addNode = Action.addNode(node);
+    const updateNode = Action.updateNode({ id: node.id, data: updatedData });
+    const updateExistingNode = [addNode, updateNode];
+    expect(updateExistingNode.reduce(reducers, undefined)).toEqual({
+      nodes: {
+        [node.id]: Object.assign({}, node, { data: updatedData }),
+      },
+      edges: {}
+    })
+
+    const badUpdate = Action.updateNode({ id: 'fakeId', data: updatedData });
+    const updateNonExistingNode = [addNode, badUpdate];
+    expect(updateNonExistingNode.reduce(reducers, undefined)).toEqual({
+      nodes: {
+        [node.id]: node,
+      },
+      edges: {}
+    })
+  });
+  it(`should handle ${ Atype.DELETE_NODE }`, () => {
+    const node = {
+      id: 'node123',
+      data: { helloWorld: 'hello' },
+    }
+    const node2 = {
+      id: 'node12',
+      data: { helloWorld: 'asdfg' },
+      incoming: ['e1'],
+      outgoing: ['e1']
+    }
+    const addNode = Action.addNode(node);
+    const addNode2 = Action.addNode(node2);
+    const deleteNode = Action.deleteNode(node);
+
+    const actions = [addNode, addNode2, deleteNode];
+
+    expect(actions.reduce(reducers, undefined)).toEqual({
+      nodes: {
+        [node2.id]: node2
+      },
+      edges: {}
+    })
+  });
 });
