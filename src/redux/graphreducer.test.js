d057a8f8e3e6614eda412a1cd308ba870e2e6b5f
winchen <wchen0@gmail.com>
2018-04-19 18:25:21 -0400

    rename graphreducer in tests

--- src/redux/graphreducer.test.js
+++ src/redux/graphreducer.test.js
@@ -1,4 +1,4 @@
-import reducers from './graphreducer';
+import graphreducer from './graphreducer';
 import Action, { Atype } from './actions';
 import expect from 'expect';
 import { testNodes, testEdges, INIT_NODE, INIT_EDGE } from './test/common';
@@ -8,7 +8,7 @@ const fillEdge = edge => Object.assign({}, INIT_EDGE, edge);
 
 describe('graph actions and reducer', () => {
   it('should return the initial state', () => {
-    expect(reducers(undefined, {})).toEqual({
+    expect(graphreducer(undefined, {})).toEqual({
       nodes: {},
       edges: {}
     })
@@ -19,7 +19,7 @@ describe('graph actions and reducer', () => {
     const expectedNode = fillNode(node);
 
     /* add a node */
-    expect(reducers(undefined, addNode)).toEqual({
+    expect(graphreducer(undefined, addNode)).toEqual({
       nodes: {
         [node.id]: expectedNode
       },
@@ -30,7 +30,7 @@ describe('graph actions and reducer', () => {
     const addTwoNodes = testNodes.slice(0, 2).map(Action.addNode);
     const expectedNodes = testNodes.slice(0, 2).map(fillNode);
 
-    expect(addTwoNodes.reduce(reducers, undefined)).toEqual({
+    expect(addTwoNodes.reduce(graphreducer, undefined)).toEqual({
       nodes: {
         [testNodes[0].id]: expectedNodes[0],
         [testNodes[1].id]: expectedNodes[1]
@@ -48,7 +48,7 @@ describe('graph actions and reducer', () => {
     const updateExistingNode = [addNode, updateNode];
 
     /* updates node */
-    expect(updateExistingNode.reduce(reducers, undefined)).toEqual({
+    expect(updateExistingNode.reduce(graphreducer, undefined)).toEqual({
       nodes: {
         [node.id]: fillNode(Object.assign({}, node, { data: updatedData })),
       },
@@ -59,7 +59,7 @@ describe('graph actions and reducer', () => {
     const updateNonExistingNode = [addNode, badUpdate];
 
     /* will do nothing if trying to update non-existent node */
-    expect(updateNonExistingNode.reduce(reducers, undefined)).toEqual({
+    expect(updateNonExistingNode.reduce(graphreducer, undefined)).toEqual({
       nodes: {
         [node.id]: fillNode(node),
       },
@@ -74,11 +74,11 @@ describe('graph actions and reducer', () => {
     const edge = testEdges[0](node.id, node2.id);
 
     const populatedState = [addNode(node), addNode(node2)]
-      .reduce(reducers, undefined);
+      .reduce(graphreducer, undefined);
 
     it('should delete node from the nodes branch', () => {
 
-      const postDelete = reducers(populatedState, deleteNode(node));
+      const postDelete = graphreducer(populatedState, deleteNode(node));
 
       expect(postDelete).toEqual({
         nodes: {
@@ -89,7 +89,7 @@ describe('graph actions and reducer', () => {
     });
     it('should delete edges that belong to the node', () => {
       const postDelete = [addEdge(edge), deleteNode(node)]
-        .reduce(reducers, populatedState);
+        .reduce(graphreducer, populatedState);
       expect(postDelete).toEqual({
         nodes: {
           [node2.id]: fillNode(node2)
@@ -105,7 +105,7 @@ describe('graph actions and reducer', () => {
     const { addNode, addEdge } = Action;
     const actions = [addNode(node1), addNode(node2), addEdge(edge)];
 
-    const updatedState = actions.reduce(reducers, undefined);
+    const updatedState = actions.reduce(graphreducer, undefined);
 
     it('should add edge to edges branch of in state tree', () => {
       expect(updatedState.edges[edge.id]).toEqual(fillEdge(edge));
@@ -120,11 +120,11 @@ describe('graph actions and reducer', () => {
     const EMPTY_STATE = { nodes: {}, edges: {} };
 
     it('should not add the edge when none of the nodes exist', () => {
-      const state = [addEdge(edge)].reduce(reducers, undefined);
+      const state = [addEdge(edge)].reduce(graphreducer, undefined);
       expect(state).toEqual(EMPTY_STATE);
     })
     it('should not add the edge when one of the nodes doesn\'t exist', () => {
-      const state = [addNode(node1), addEdge(edge)].reduce(reducers, undefined);
+      const state = [addNode(node1), addEdge(edge)].reduce(graphreducer, undefined);
       expect(state).toEqual({
         nodes: { [node1.id]: fillNode(node1) },
         edges: {}
@@ -132,7 +132,7 @@ describe('graph actions and reducer', () => {
     })
     it('should not add the edge when an edge with the same id already exists', () => {
       const anotherEdge = Object.assign({}, edge, { data: 'new edge with same id' });
-      const state = reducers(updatedState, addEdge(anotherEdge));
+      const state = graphreducer(updatedState, addEdge(anotherEdge));
       expect(updatedState.edges[edge.id]).toEqual(fillEdge(edge));
       expect(updatedState.nodes[node1.id].outgoing)
         .toEqual(expect.arrayContaining([edge.id]));
@@ -151,24 +151,24 @@ describe('graph actions and reducer', () => {
     const { addNode, addEdge, deleteEdge } = Action;
 
     const actions = [node1, node2, node3].map(addNode)
-    const populatedState = actions.reduce(reducers, undefined);
+    const populatedState = actions.reduce(graphreducer, undefined);
 
     it('should remove the edge from the edges branch', () => {
       const postRemove = [addEdge(edge), deleteEdge(edge)]
-        .reduce(reducers, populatedState);
+        .reduce(graphreducer, populatedState);
       expect(Object.keys(postRemove.edges)).toEqual([]);
     });
     it('should remove edge from its nodes incoming and outgoing', () => {
       const postRemove = [addEdge(edge), deleteEdge(edge)]
-        .reduce(reducers, populatedState);
+        .reduce(graphreducer, populatedState);
       expect(postRemove.nodes[node1.id].outgoing).toEqual(fillNode(node1).outgoing);
       expect(postRemove.nodes[node2.id].incoming).toEqual(fillNode(node2).incoming);
     });
     it('should not delete anything else', () => {
       const postRemove = [addEdge(edge), addEdge(edge2), deleteEdge(edge)]
-        .reduce(reducers, populatedState);
+        .reduce(graphreducer, populatedState);
 
-      const stateWithoutEdge = reducers(populatedState, addEdge(edge2));
+      const stateWithoutEdge = graphreducer(populatedState, addEdge(edge2));
       expect(postRemove).toEqual(stateWithoutEdge);
     });
     it('should not delete anything if edge does not exist', () => {});
@@ -182,7 +182,7 @@ describe('graph actions and reducer', () => {
     const edge = testEdges[0](node1.id, node2.id);
 
     const populatedState = [addNode(node1), addNode(node2)]
-      .reduce(reducers, undefined);
+      .reduce(graphreducer, undefined);
 
     const stateWithEdges =
     it('should update edges', () => {
@@ -193,9 +193,9 @@ describe('graph actions and reducer', () => {
       const edgeWithUpdate = Object.assign({}, edge, newData);
 
       const edgeUpdate = [addEdge(edge), updateEdge(newData)]
-        .reduce(reducers, populatedState);
+        .reduce(graphreducer, populatedState);
 
-      const builtFromScratch = reducers(populatedState, addEdge(edgeWithUpdate));
+      const builtFromScratch = graphreducer(populatedState, addEdge(edgeWithUpdate));
 
       expect(edgeUpdate).toEqual(builtFromScratch);
     })
