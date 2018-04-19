8607600697108c8d098753c56dda36ec8f433be6
winchen <wchen0@gmail.com>
2018-04-19 18:24:37 -0400

    write graphreducer factory

--- src/redux/graphreducer.js
+++ src/redux/graphreducer.js
@@ -1,7 +1,9 @@
-import { combineReducers } from 'redux';
 import Action, { Atype } from './actions';
 
 const INIT_STATE = { nodes: {}, edges: {} };
+
+const OUTGOING = 'outgoing';
+const INCOMING = 'incoming';
 
 const initNode = (
   id = undefined,
@@ -17,30 +19,41 @@ const initEdge = (
   destin = undefined
 ) => ({ id, data, origin, destin });
 
-/* NOTE: placeholder. replace with pluggable data reducers */
-const dataUpdaterPlaceholder = (data = {}, action) => {
+const defaultNodeDataReducer = (data, action) => {
   switch (action.type) {
     case Atype.UPDATE_NODE: {
       return action.data;
     }
+    default: return data
+  }
+};
+const defaultEdgeDataReducer = (data, action) => {
+  switch (action.type) {
     case Atype.UPDATE_EDGE: {
       return action.data;
     }
-    default: return data;
+    default: return data
   }
-}
+};
+const defaultEdgeListReducer = (edgeList = [], action) => {
+  const { id } = action;
+  switch (action.type) {
+    case Atype.ADD_EDGE: {
+      return edgeList.concat(id)
+    }
+    case Atype.DELETE_EDGE: {
+      const newList = edgeList.slice();
+      newList.splice(newList.indexOf(id), 1);
+      return newList;
+    }
+    default: return node
+  }
+};
 
-const insertEdge = (node, direction, id) => Object.assign({}, node,
-  { [direction]: node[direction].concat(id) }
-);
-const spliceEdge = (node, direction, id) => {
-  const newList = node[direction].slice();
-  newList.splice(newList.indexOf(id), 1);
-
-  return Object.assign({}, node, { [direction]: newList });
-}
-
-const nodes = (state = {}, action) => {
+const nodesReducerFactory = (
+  dataReducer = defaultNodeDataReducer,
+  edgeListReducer = defaultEdgeListReducer
+) => (state = {}, action) => {
   switch (action.type) {
     case Atype.ADD_NODE: {
       const { id, data, incoming, outgoing } = action;
@@ -51,7 +64,7 @@ const nodes = (state = {}, action) => {
     case Atype.UPDATE_NODE: {
       const { id, data } = action;
       let node = state[id];
-      if (node) node = Object.assign({}, node, { data: dataUpdaterPlaceholder(node.data, action) });
+      if (node) node = Object.assign({}, node, { data: dataReducer(node.data, action) });
       return Object.assign({}, state, { [id]: node });
     }
     case Atype.CLEAR_NODE: {
@@ -59,87 +72,94 @@ const nodes = (state = {}, action) => {
       delete nState[action.id];
       return nState;
     }
-    case Atype.ADD_EDGE: {
-      const { origin, destin, id } = action;
-      const originNode = state[origin];
-      const destinNode = state[destin];
-      return Object.assign({}, state, {
-        [origin]: insertEdge(originNode, 'outgoing', id),
-        [destin]: insertEdge(destinNode, 'incoming', id),
-      })
-    }
+    case Atype.ADD_EDGE:
     case Atype.DELETE_EDGE: {
       const { origin, destin, id } = action;
       const originNode = state[origin];
       const destinNode = state[destin];
+
+      const edgeFrom = Object.assign({}, action, { direction: OUTGOING });
+      const edgeTo = Object.assign({}, action, { direction: INCOMING });
+
+      const outgoing = edgeListReducer(originNode.outgoing, edgeFrom);
+      const incoming = edgeListReducer(destinNode.incoming, edgeTo);
+
       return Object.assign({}, state, {
-        [origin]: spliceEdge(originNode, 'outgoing', id),
-        [destin]: spliceEdge(destinNode, 'incoming', id),
+        [origin]: Object.assign({}, originNode, { outgoing }),
+        [destin]: Object.assign({}, destinNode, { incoming }),
       })
     }
     default: return state;
   }
 }
 
-const edges = (state = {}, action) => {
-  switch (action.type) {
-    case Atype.ADD_EDGE: {
-      const { id, data, origin, destin } = action;
-      const edge = initEdge(id, data, origin, destin);
+const edgesReducerFactory = (dataReducer = defaultEdgeDataReducer) =>
+  (state = {}, action) => {
+    switch (action.type) {
+      case Atype.ADD_EDGE: {
+        const { id, data, origin, destin } = action;
+        const edge = initEdge(id, data, origin, destin);
 
-      return Object.assign({}, state, { [id]: edge });
+        return Object.assign({}, state, { [id]: edge });
+      }
+      case Atype.UPDATE_EDGE: {
+        const { id, data } = action;
+        let edge = state[id];
+        if (edge) edge = Object.assign({}, edge, { data: dataReducer(edge.data, action) });
+        return Object.assign({}, state, { [id]: edge});
+      }
+      case Atype.DELETE_EDGE: {
+        const nState = Object.assign({}, state);
+        delete nState[action.id];
+        return nState;
+      }
+      default: return state;
     }
-    case Atype.UPDATE_EDGE: {
-      const { id, data } = action;
-      let edge = state[id];
-      if (edge) edge = Object.assign({}, edge, { data: dataUpdaterPlaceholder(edge.data, action) });
-      return Object.assign({}, state, { [id]: edge});
-    }
-    case Atype.DELETE_EDGE: {
-      const nState = Object.assign({}, state);
-      delete nState[action.id];
-      return nState;
-    }
-    default: return state;
   }
-}
 
-function graphreducer(state = INIT_STATE, action) {
-  // pack data from state onto actions here.
-  let packOntoAction = {};
+export const graphReducerFactory = (nodeDataR, edgeListR, edgeDataR) => {
+  const nodes = nodesReducerFactory(nodeDataR, edgeListR);
+  const edges = edgesReducerFactory(edgeDataR);
 
-  switch (action.type) {
-    case Atype.ADD_EDGE: {
-      const { origin, destin, id } = action;
-      const { nodes = {}, edge = {} } = state;
+  return (state = INIT_STATE, action) => {
+    // pack data from state onto actions here.
+    let packOntoAction = {};
 
-      if (edges[id] || !nodes[origin] || !nodes[destin]) return state;
-      break;
-    }
-    case Atype.DELETE_EDGE: {
-      const edge = state.edges[action.id];
-      if (edge) packOntoAction = edge;
-      break;
-    }
-    case Atype.DELETE_NODE: {
-      const node = state.nodes[action.id];
-      if (node) {
-        const { outgoing, incoming } = node;
-        const deleteEdgesThenNode = outgoing.concat(incoming)
-          .map(id => Action.deleteEdge(state.edges[id]))
-          .concat(Action.clearNode(node));
+    switch (action.type) {
+      case Atype.ADD_EDGE: {
+        const { origin, destin, id } = action;
+        const { nodes = {}, edge = {} } = state;
 
-        return deleteEdgesThenNode
-          .reduce((state, action) => {
-            return graphreducer(state, action)
-          }, state);
+        if (edges[id] || !nodes[origin] || !nodes[destin]) return state;
+        break;
+      }
+      case Atype.DELETE_EDGE: {
+        const edge = state.edges[action.id];
+        if (edge) packOntoAction = edge;
+        break;
+      }
+      case Atype.DELETE_NODE: {
+        const node = state.nodes[action.id];
+        if (node) {
+          const { outgoing, incoming } = node;
+          const deleteEdgesThenNode = outgoing.concat(incoming)
+            .map(id => Action.deleteEdge(state.edges[id]))
+            .concat(Action.clearNode(node));
+
+          return deleteEdgesThenNode
+            .reduce((state, action) => {
+              return graphreducer(state, action)
+            }, state);
+        }
       }
     }
-  }
-  return {
-    nodes: nodes(state.nodes, Object.assign({}, action, packOntoAction)),
-    edges: edges(state.edges, Object.assign({}, action, packOntoAction)),
+    return {
+      nodes: nodes(state.nodes, Object.assign({}, action, packOntoAction)),
+      edges: edges(state.edges, Object.assign({}, action, packOntoAction)),
+    }
   }
 }
+
+const graphreducer = graphReducerFactory();
 
 export default graphreducer;
