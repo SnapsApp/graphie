4d108ca3010707ad839ef83a5e722b6328b9cf50
winchen <wchen0@gmail.com>
2018-06-14 15:17:17 -0400

    move fauxschema to common

--- src/redux/schemaReducer/schemaReducer.test.js
+++ src/redux/schemaReducer/schemaReducer.test.js
@@ -1,48 +1,14 @@
 import expect from 'expect';
 import { generateEntityFromSchema } from './schemaGetters';
 import schemaReducer from './index';
+import { fauxSchema } from '../vsReducer/common';
 
 describe('schema getters', () => {
   it('should correctly initialize a blank entity from a schema', () => {
-    const fauxSchema = {
-      contractAttributes: {
-        active: {
-          type: "boolean",
-          transform: "booleanize",
-          defaultsTo: false
-        },
-        appId: { type: "string" },
-        blacklistUsers: {
-          type: "array",
-          description: "A list of Snaps user IDs to hide this report for."
-        },
-        detailType: { type: "string" },
-        displayIcon: {
-          type: "string",
-          required: true,
-          defaultsTo: "ion-ionic"
-        },
-        id: { type: "string" },
-        name: {
-          type: "string",
-          required: true
-        },
-        meta: {
-          type: "object"
-        },
-      }
-    }
-    const expectedEntity = {
-      active: false,
-      appId: undefined,
-      blacklistUsers: undefined,
-      detailType: undefined,
-      displayIcon: "ion-ionic",
-      id: undefined,
-      name: undefined,
-      meta: undefined
-    }
-    const emptyEntity = generateEntityFromSchema(fauxSchema);
+    const schema = fauxSchema.schema;
+    const expectedEntity = fauxSchema.defaultEntity;
+
+    const emptyEntity = generateEntityFromSchema(schema);
     expect(emptyEntity).toEqual(expectedEntity);
   })
 })
